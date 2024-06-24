f86fe48964cd908d39a2a03f29c3730aa1179415
Marcos Gomes <marcosartemio221@gmail.com>
2024-06-24 18:26:50 -0300

    Initial commit

---
+++ src/main/java/sistema/de/vendas/VendasApplication.java
@@ -0,0 +1,13 @@
+package sistema.de.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasApplication.class, args);
+	}
+
+}
