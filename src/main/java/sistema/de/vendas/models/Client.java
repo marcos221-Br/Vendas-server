067efcbb55b60a8d77b73ce4c324ef68e5f7e09c
Marcos Gomes <marcosartemio221@gmail.com>
2024-06-25 17:19:18 -0300

    Added Client
    Added Client Class

--- src/main/java/sistema/de/vendas/models/Client.java
+++ src/main/java/sistema/de/vendas/models/Client.java
@@ -11,15 +11,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users")
-public class User implements Serializable{
+@Table(name = "clients")
+public class Client implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
-    @Column(name = "iduser")
+    @Column(name = "idclient")
     private Integer id;
 
     @Basic(optional = false)
@@ -27,18 +27,18 @@ public class User implements Serializable{
     private String name;
 
     @Basic(optional = false)
-    @Column(name = "password")
-    private String password;
+    @Column(name = "cellphone")
+    private String cellphone;
 
-    public User(Integer id, String name, String password){
+    public Client(Integer id, String name, String cellphone){
         this.id = id;
         this.name = name;
-        this.password = password;
+        this.cellphone = cellphone;
     }
 
-    public User(String name, String password){
+    public Client(String name, String cellphone){
         this.name = name;
-        this.password = password;
+        this.cellphone = cellphone;
     }
 
     public Integer getId(){
@@ -57,16 +57,16 @@ public class User implements Serializable{
         this.name = name;
     }
 
-    public String getPassword() {
-        return this.password;
+    public String getCellphone() {
+        return this.cellphone;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
     }
 
     @Override
     public String toString() {
-        return "User\nId: " + this.id + "\nName: " + this.name + "\nPassword: " + this.password;
+        return "Client\nId: " + this.id + "\nName: " + this.name + "\nCellphone: " + this.cellphone;
     }
 }
